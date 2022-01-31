0bfbe2900b0e60dfc2bd4f2b372e4b888c5fc60b
Eric Murat <eric.murat@berger-levrault.com>
2022-01-31 14:02:45 +0100

    Callback et paramètres

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('button');
 
 function fToggle() {
-    console.log("Click du button --> button.classList.toggle(..)");
+    console.log("Click du button.");
     button.classList.toggle('altColor');
 }
 
@@ -9,9 +9,38 @@ function fToggle() {
 //fToggle();
 
 // Utilisation d'un callback - OK
-button.addEventListener('click', fToggle);
+//button.addEventListener('click', fToggle);
 
-// Utilisation d'un appel direct ! KO !!!
+// Utilisation d'un appel direct ! KO !!! La fonction est déclenchée lors de l'évaluation
 //button.addEventListener('click', fToggle());
 
-// Comment passer les paramètres à une fonction de callback ?
+// Autre syntaxe OK
+// button.addEventListener('click', function() {
+//     console.log("Click du button.");
+//     button.classList.toggle('altColor');
+// });
+// Autre syntaxe OK
+button.addEventListener('click', () => {
+    console.log("Click du button.");
+    button.classList.toggle('altColor');
+});
+
+// ----------------------------------
+// ASYNC +  Comment passer les paramètres à une fonction de callback ?
+// ----------------------------------
+function firstAction(callback) {
+    console.log("First action");
+    setTimeout(callback, 2000);
+}
+
+function secondAction() {
+    console.log("Second action");
+}
+
+//setTimeout(firstAction(secondAction), 5000);
+// KO!!!  "First action" est affiché instantanément car évalué !
+
+// Solution : utiliser "() => " fonction anonyme de callback
+setTimeout(() => firstAction(secondAction), 5000);
+
+
